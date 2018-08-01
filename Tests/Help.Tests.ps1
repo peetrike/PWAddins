08cedefcc824f179caafcdb9d75766352cc55a0f
Meelis Nigols <meelis.nigols@telia.ee>
2018-08-01 15:03:44 +0300

    corrected command case

--- Tests/Help.Tests.ps1
+++ Tests/Help.Tests.ps1
@@ -100,7 +100,7 @@ foreach ($command in $commands) {
             foreach ($link in $links) {
                 if ($link) {
                     # Should have a valid uri if one is provided.
-                    it "[$link] should have 200 Status Code for $commandName" {
+                    It "[$link] should have 200 Status Code for $commandName" {
                         $Results = Invoke-WebRequest -Uri $link -UseBasicParsing
                         $Results.StatusCode | Should Be '200'
                     }
