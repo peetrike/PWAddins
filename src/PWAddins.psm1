3027e98579601b7ce8a9a1be6f42bdd69b006cf4
Peter Wawa <meelisn@outlook.com>
2023-07-27 11:26:27 +0300

    Split module initialization to separate script

--- src/PWAddins.psm1
+++ src/PWAddins.psm1
@@ -1,19 +1,6 @@
-<#
-    .Synopsis
-    Module that contains additional useful functions for command line
-
-    .Description
-    Some tweaks for command-line environment.
-
-    .NOTES
-    Author: Peter Wawa
-
-#>
 # Set-StrictMode -Version Latest
 
-Write-Verbose 'Initializing module PWAddins'
-
-$Public  = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue )
+$Public = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue )
 $Private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue )
 
 #Dot source the files
@@ -27,18 +14,4 @@ foreach ($import in @($Public + $Private)) {
 
 foreach ($Function in $Public) {
     Export-ModuleMember -Function $Function.BaseName
-}
-
-if ($PSVersionTable.PSVersion.Major -lt 6) {
-    $FileNamePart = join-path -Path $PSScriptRoot -ChildPath 'HistoryInfo'
-    if (-not (Get-TypeData -TypeName Microsoft.PowerShell.Commands.HistoryInfo).Members.Duration) {
-        Update-TypeData -PrependPath ($FileNamePart + '.types.ps1xml')
-    }
-    if (
-        (
-            Get-FormatData -TypeName Microsoft.PowerShell.Commands.HistoryInfo
-        ).FormatViewDefinition.Name -notcontains 'History.Table'
-    ) {
-        Update-FormatData -PrependPath ($FileNamePart + '.format.ps1xml')
-    }
 }
---
+++ src/_Init.ps1
@@ -0,0 +1,13 @@
+﻿if ($PSVersionTable.PSVersion.Major -lt 6) {
+    $FileNamePart = Join-Path -Path $PSScriptRoot -ChildPath 'HistoryInfo'
+    if (-not (Get-TypeData -TypeName Microsoft.PowerShell.Commands.HistoryInfo).Members.Duration) {
+        Update-TypeData -PrependPath ($FileNamePart + '.types.ps1xml')
+    }
+    if (
+        (
+            Get-FormatData -TypeName Microsoft.PowerShell.Commands.HistoryInfo
+        ).FormatViewDefinition.Name -notcontains 'History.Table'
+    ) {
+        Update-FormatData -PrependPath ($FileNamePart + '.format.ps1xml')
+    }
+}
