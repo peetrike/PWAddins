f9829a6c1805ed29be93c17c55d9d8edb34b2db5
Peter Wawa <meelisn@outlook.com>
2025-08-26 18:35:22 +0300

    Add Start-PSContainer function

---
+++ src/Public/Start-PSContainer.ps1
@@ -0,0 +1,30 @@
+﻿function Start-PSContainer {
+    # .EXTERNALHELP PWAddins-help.xml
+    [OutputType([void])]
+    [Alias('Start-LinuxPS')]
+    [CmdletBinding()]
+    param (
+            [Parameter(
+                Mandatory
+            )]
+            [string]
+        $ImageName
+    )
+
+    function Get-PodmanMachine {
+        podman machine list --format json | ConvertFrom-Json
+    }
+
+    $Machine = Get-PodmanMachine
+    while ($Machine.Starting) {
+        Write-Verbose -Message 'Podman machine is still starting'
+        Start-Sleep -Seconds 5
+        $Machine = Get-PodmanMachine
+    }
+
+    if (-not $Machine.Running) {
+        podman machine start
+    }
+
+    podman run --rm -it "localhost/$ImageName"
+}
